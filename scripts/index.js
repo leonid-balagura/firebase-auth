d91358080e23b3ced55a41f9af0f033596121d4b
Leonid Balagura <leonidbalagura@gmail.com>
2021-01-10 23:59:31 +0200

    get data from firestore; add logic to show data only for registered users

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,31 @@
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
+// setup guides
+const guidesList = document.querySelector(".guides");
 
-  var modals = document.querySelectorAll('.modal');
+const setupGuides = (docs) => {
+  if (docs.length) {
+    let html = "";
+    docs.forEach((doc) => {
+      const { title, content } = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4">${title}</div>
+          <div class="collapsible-body white">${content}</div>
+        </li>
+      `;
+      html += li;
+    });
+
+    guidesList.innerHTML = html;
+  } else {
+    guidesList.innerHTML = `<h5 class="center-align">Login to view guides</h5>`;
+  }
+};
+
+// setup materialize components
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
   M.Modal.init(modals);
 
-  var items = document.querySelectorAll('.collapsible');
+  var items = document.querySelectorAll(".collapsible");
   M.Collapsible.init(items);
-
 });
